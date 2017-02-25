31416e88fa5e77a9edda23d1e617da5f65ed9fb4
juan_linares <linares.0@hotmail.com>
2017-02-25 09:14:13 -0500

    se definireron 2 rutas de contenido estatico

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hola/index','sitioController@index');
+
+Route::get('hola/saludo/{nombre}','sitioController@saludar');
